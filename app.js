b433e71755b41902a33ef99c48febd4bd635f313
Nikita Chernyshov <xpom55@mail.ru>
2018-03-29 16:58:33 +0300

    blablabla a lot of information about decorator =)

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function logged(consctructionFn) {
     console.log(consctructionFn);
 }
@@ -16,7 +19,8 @@ var Person = /** @class */ (function () {
     ], Person);
     return Person;
 }());
-// Factory
+// Factory // разница только в том, что в factory можно передать параметры
+// но вроде как нужно обратно вернуть обязательно декоратор или null
 function logging(value) {
     return value ? logged : null;
 }
@@ -51,6 +55,67 @@ var Plant = /** @class */ (function () {
     return Plant;
 }());
 var plant = new Plant();
-// plant.print(); // error print didn't exist
+// plant.print(); // error print didn't exist, ts bugs
 plant.print();
 plant.print2();
+// Method Decorator
+// Property decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+function overwritable(value) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    // @editable(false)
+    Project.prototype.calcBudget = function () {
+        console.log(1000);
+    };
+    __decorate([
+        overwritable(false) // false ничего нельзя заменить в классе и он пустой будет
+    ], Project.prototype, "projectName");
+    return Project;
+}());
+var project = new Project("super project");
+project.calcBudget();
+project.calcBudget = function () {
+    console.log(2000);
+};
+project.calcBudget();
+console.log(project);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log('target: ', target);
+    console.log('methodName: ', methodName);
+    console.log('paramIndex: ', paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumber = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumber");
+    return Course;
+}());
+var course = new Course('Super course');
+course.printStudentNumber('anything', true);
+course.printStudentNumber('anything', false);
--- app.ts
+++ app.ts
@@ -9,7 +9,8 @@ class Person {
   }
 }
 
-// Factory
+// Factory // разница только в том, что в factory можно передать параметры
+// но вроде как нужно обратно вернуть обязательно декоратор или null
 function logging(value: boolean) {
   return value ? logged : null;
 }
@@ -41,6 +42,74 @@ class Plant {
 }
 
 const plant = new Plant();
-// plant.print(); // error print didn't exist
+// plant.print(); // error print didn't exist, ts bugs
 (<any>plant).print();
 (<any>plant).print2();
+
+
+// Method Decorator
+// Property decorator
+function editable(value: boolean) {
+  return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+    descriptor.writable = value;
+  }
+}
+
+function overwritable(value: boolean) {
+  return function(target: any, propName: string): any {
+    const newDescriptor: PropertyDescriptor = {
+      writable: value
+    };
+
+    return newDescriptor;
+  }
+}
+
+class Project {
+  @overwritable(false) // false ничего нельзя заменить в классе и он пустой будет
+  projectName: string;
+
+  constructor(name: string) {
+    this.projectName = name;
+  }
+
+  // @editable(false)
+  calcBudget() {
+    console.log(1000);
+  }
+}
+
+const project = new Project("super project");
+project.calcBudget();
+project.calcBudget = function() {
+  console.log(2000);
+};
+project.calcBudget();
+console.log(project);
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log('target: ', target);
+  console.log('methodName: ', methodName);
+  console.log('paramIndex: ', paramIndex);
+}
+
+class Course {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  printStudentNumber(mode: string, @printInfo printAll: boolean) {
+    if (printAll) {
+      console.log(10000);
+    } else {
+      console.log(2000);
+    }
+  }
+}
+
+const course = new Course('Super course');
+course.printStudentNumber('anything', true);
+course.printStudentNumber('anything', false);
